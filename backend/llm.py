77ab5d7e5bf860373b7d4ddd128ff2f4f90306b0
Matheus Gominho <matheus.rosa@di2win.com>
2025-03-07 04:42:03 -0300

    Patch: Changing from local model to google ai studio

--- backend/llm.py
+++ backend/llm.py
@@ -1,10 +1,26 @@
-from langchain_ollama import ChatOllama
+from langchain_google_genai import GoogleGenerativeAI
+import os
 
 class MyLLM:
-    def __init__(self, local_model = 'llama3.1:8b'):
-        self.chat = ChatOllama(model = local_model)
+    def __init__(self, google_model = 'gemini-2.0-flash'):
+        self.api_key = os.environ.get('API_KEY')
+        self.model = google_model
+        self.chat = GoogleGenerativeAI(model = self.model, google_api_key = self.api_key)
+        self.background = None
+
+    def get_background(self, background):
+        self.background = background
+        return self
 
     def invoke(self, contexts, prompt:str):
+        if self.background:
+            template = f'''
+            {self.background},
+            {contexts},
+            agora responda a seguinte pergunta:
+            {prompt}
+            '''
+            return self.chat.invoke(template).content
         template = f'''
         You are a machine learning specialist and you are inserted in the following context
         {contexts}
