27f8490b65eb56288842855ac3fb9561b016b70e
Matheus Gominho <matheus.rosa@di2win.com>
2025-03-07 07:17:00 -0300

    Post: Adding main pdf processor class to the dags

--- airflow/dags/pdfreader.py
+++ airflow/dags/pdfreader.py
@@ -1,32 +1,28 @@
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
 import os
 
 class Reader:
     def __init__(self, path_to_pdfs, api_key):
         self.api_key = os.environ.get('API_KEY')
-        self.path = path_to_pdfs
+        self.path = os.environ.get('PDF_PATH')
         self.texts = []
-        self.chunks = []
 
-    def get_texts(self, collection):
+    def get_texts(self, filename):
+        full_path = os.path.join(self.path, filename)
         text = ''
-        for pdf in collection:
-            pdf_reader = PdfReader(pdf)
-            for page in pdf.reader_pages:
-                text += page.extract_text()
+
+        pdf_reader = PdfReader(full_path)
+        
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
         self.texts.append(text)
+
         return self
 
-    def get_text_chunk(self, text):
-        text_splitter = CharacterTextSplitter(
-            separators = '\n',
-            chunk_size = 10000,
-            chunk_overlap = 200,
-            length_function = len
-        )
-
-        self.chunks = text_splitter
+    def workflow(self):
+        all_pdfs = os.listdir(self.path)
+        for pdf in all_pdfs:
+            self.get_texts(pdf)
+        
         return self
