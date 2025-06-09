986cd3f4fffb4a3e8c54f0d95e7f6b613a1af98b
Matheus Rosa <matheusnsampaio@gmail.com>
2025-06-09 04:46:20 -0300

    :hammer: overhaul on structuring of files and also the start remade http methods

---
+++ frontend/APIClient.py
@@ -0,0 +1,70 @@
+import httpx
+import json
+from schema import TextInput, Question, ListTextInput, QueryText, Background
+
+class APIClient:
+    def __init__(self, port='8000'):
+        self.client = httpx.Client(timeout = 300)
+        self.base_url = f'http://backend:{port}'
+        self.getter = '/context/get'
+        self.poster = '/context/post'
+        self.background = '/background'
+        self.all = '/context/get/all'
+        self.llm = '/llm/response'
+
+    def post(self, text_input):
+        if not isinstance(text_input, list):
+            raise ValueError("Input must be a list of dictionaries")
+
+        response = self.client.post(
+            self.base_url + self.poster,
+            json = text_input,
+        )
+        
+        response.raise_for_status()
+        return response.json()
+
+    def get(self, query):
+        if not isinstance(query, dict):
+            raise ValueError("Query must be a dictionary")
+
+        query = QueryText(**query)
+
+        response = self.client.request(
+            method = 'GET',
+            url = self.base_url + self.getter,
+            json = query.model_dump()
+        )
+        
+        response.raise_for_status()
+        return response.json()
+
+    def post_background(self, background):
+        response = self.client.post(
+            self.base_url + self.background,
+            json = background
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def get_all(self):
+        response = self.client.request(
+            method = 'GET',
+            url = self.base_url + self.all,
+        )
+
+        response.raise_for_status()
+        return response.json()
+
+    def get_llm_response(self, question):
+        if not isinstance(question, dict):
+            raise ValueError("Question must be a dictionary")
+       
+        response = self.client.request(
+            method = 'GET',
+            url = self.base_url + self.llm,
+            json = question
+        )
+        
+        response.raise_for_status()
+        return response.json()
---
+++ frontend/st.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import slate3k as slate 
+from APIClient import APIClient
+from schemas.schema import Background, Question
+
+client = APIClient()
+
+st.set_page_config(layout='wide')
+
+with st.sidebar:
+    st.markdown('# Janela de contexto')
+    st.markdown('## Adicione a base')
+    
+    with st.form(key='my_form'):
+        id_input = st.text_area(label='Id', value='Insira um id unico para o contexto')
+        text_input = st.file_uploader('Envie seu pdf para adicionar contexto a base', type="pdf")
+        submit_context = st.form_submit_button(label='submit')
+
+        if submit_context:
+            try:
+                text_input = slate.PDF(text_input)
+                text_input = ' '.join(text_input)
+                text_inputs = {'query_id': id_input, 'text': text_input}
+                response = client.post([text_inputs])
+                st.success("Contexto adicionado com sucesso.")
+            except Exception as e:
+                st.error(f"Error adding context: {e}")
+        
+        st.markdown('---')
+        st.markdown('## Procure na base de dados')
+    
+    with st.form(key='get_context'):
+        get_text = st.text_area(label='Procurar contexto', value='Procure na base')
+        get_n_responses = st.number_input('Insira quantas respostas deseja', value = 1)
+        get_context = st.form_submit_button(label='submit')
+        
+        if get_context:
+            try:
+                get_texts = {'query': get_text, 'n_results':get_n_responses}
+                context_response = client.get(get_texts)
+                st.text(context_response)
+            except Exception as e:
+                st.error(f"Error retrieving context: {e}")
+
+    with st.form(key = 'get_all'):
+        get_all = st.form_submit_button(label = 'submit')
+
+        if get_all:
+            st.text(client.get_all())
+
+with st.form('create_background'):
+    st.markdown('## Escolha o background para o modelo')
+    background_input = st.text_area(label='Background', value='Escolha um background para o modelo')
+    submit_bg = st.form_submit_button(label='submit')
+    
+if submit_bg:
+    background_obj = Background(bg = background_input)
+    client.post_background(background=background_obj.model_dump())
+    st.success('Background adicionado com sucesso.')
+
+st.markdown('# Janela de resposta')
+column1, column2 = st.columns(2)
+
+with st.form('get_response'):
+    with column1:
+        st.markdown('## Pergunta passada para a llm')
+        llm_input = st.text_area(label='Pergunta', value='Insira sua pergunta aqui')   
+
+    with column2:
+        st.markdown('## Insira aqui a quantidade de contextos necessarios')
+        n_contexts = st.number_input(label='Quantidade', value=1)
+    
+    submit = st.form_submit_button(label = 'submit')
+
+if submit:
+    try:
+        question = {'question': llm_input, 'n_contexts': n_contexts}
+        llm_response = client.get_llm_response(Question(**question).model_dump())
+        st.text(llm_response)
+    except Exception as e:
+        st.error(f"Error getting LLM response: {e}")
